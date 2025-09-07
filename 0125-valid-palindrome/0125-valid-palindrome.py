142e3ed5f221b9597738ddb34739f9069151bd64
Denys Tsinyk <denystsinyk@gmail.com>
2025-09-07 17:14:57 -0400

    Time: 8 ms (52.92%), Space: 18.4 MB (40.59%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,18 +1,20 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        left = 0
-        right = len(s) - 1
+        cleaned_string = "".join(char for char in s if char.isalnum()).lower()
+        # .isalnum() checks if anything is alpha numeric
+        # char for char in s = takes each character in s 
+        # if in char.isalum then joins to the string
+        # .lower to account for capitals
 
-        while left < right:
-            while left < right and not s[left].isalnum():
-                left += 1
-            while left < right and not s[right].isalnum():
+        left, right = 0, len(cleaned_string) - 1 # since len() gives num of chars need
+        # to convet tho index form ... ie. -1
+        while left < right: # no need to use a for loop since the 2 points converge
+        # we want to keep doing this until they touch, but not == bc they can skip
+            if cleaned_string[left] == cleaned_string[right]: 
+                # check opposite ends to make sure they match
+                left += 1 
                 right -= 1
-
-            if s[left].lower() != s[right].lower():
+                # shift each inside
+            else:
                 return False
-
-            left += 1
-            right -= 1
-
         return True
