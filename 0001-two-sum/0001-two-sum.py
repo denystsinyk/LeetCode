98592801cb88f21a910e6a18170bbaf673db281a
Denys Tsinyk <denystsinyk@gmail.com>
2025-09-07 16:44:57 -0400

    Time: 0 ms (100%), Space: 19.1 MB (10.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
-        for i in range(len(nums)):
-            dif = target - nums[i]
-            if dif in d:
-                return [i,d[dif]]
-            else:
-                d[nums[i]] = i
-
         
+        seen = {} # using a dictionary to record all the values we have seen as looping
+        # also keeps track of those index
+
+        for index, value in enumerate(nums): # loop through keeps track of value and index
+            diff = target - value # difference is useful to determine what number we need
+            if diff in seen: # if we have seen it then we can return
+                return [seen[diff],index]
+            seen[value] = index # if we havent seen it yet then add to the seen
+
+            # we want to add the value map to index since we also want the diff to match
+            # wont be possible to map the other way around
