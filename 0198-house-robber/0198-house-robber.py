770fc616546bd8c1640311ac84e9aad1e5b30759
Denys Tsinyk <denystsinyk@gmail.com>
2025-11-11 13:10:37 -0500

    Time: 0 ms (100%), Space: 17.7 MB (82.13%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # max val
+        # pass in index instead if an array to know where t start
+        # decision tree allows for dp
+        # once i = len(nums), update our max
+
+        
+        dp = [0] * (len(nums) + 1)
+        dp[1] = nums[0]
+
+        for i in range(2, len(nums) + 1):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])
+
+        return dp[-1]
+
+        
+        
+
+
+
+    
