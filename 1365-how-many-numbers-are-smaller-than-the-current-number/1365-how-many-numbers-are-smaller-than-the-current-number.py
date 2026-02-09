4493e2b2d7bb23c2cc5428c0ce5256c9532d12ad
Denys Tsinyk <denystsinyk@gmail.com>
2026-02-09 12:55:04 -0500

    Time: 0 ms (100%), Space: 19.3 MB (34.72%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,19 +1,45 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        freq = [0] * 101
-        result = []
 
-        for value in nums:
-            freq[value] += 1
+        # loop through each num
+        # for each num run another loop and if smaller add one
+        # we could also sort it, then anything after is larger
+        # since we cant use a dict we could also do a counter
 
-        smaller_count = [0] * 101
-        running = 0
+        # res = [0] * len(nums) # this did not speed it up
 
-        for v in range(len(smaller_count)):
-            smaller_count[v] = running
-            running += freq[v]
+        # for i in range(len(nums)):
+        #     count = 0
+        #     for j in range(len(nums)):
+        #         if i != j and nums[j] < nums[i]: 
+        #             count += 1
+        #     res[i] = count
+        # return res
 
-        for value in nums:
-            result.append(smaller_count[value])
+        # to make it faster we could sort it then get the first
+        # occurance and that index is how many nums are smaller than it
+        counts = {}
+        sorted_nums = sorted(nums)
 
-        return result
+        for i in range(len(nums)):
+            if sorted_nums[i] not in counts:
+                counts[sorted_nums[i]] = i
+        # basically if the num is not in the dictionary, 
+        # then we set its value to the index, bc thats how many are smaller than it
+        res = []
+        for num in nums:
+            count = counts[num] # we get the count that are smaller
+            res.append(count)
+            # we dont have to worry abt it being out of order bc we just loops through nums
+            # and then like count is the nums so it works out
+        return res
+        
+
+
+
+        
+        
+
+
+
+        
