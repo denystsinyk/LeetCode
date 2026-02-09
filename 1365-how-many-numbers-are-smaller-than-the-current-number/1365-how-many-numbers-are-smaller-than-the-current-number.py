3ac47750c6bbdd4982680e92d85797d856c2833f
Denys Tsinyk <denystsinyk@gmail.com>
2026-02-09 12:38:26 -0500

    Time: 184 ms (9.75%), Space: 19.5 MB (13.6%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -6,15 +6,20 @@ class Solution:
         # we could also sort it, then anything after is larger
         # since we cant use a dict we could also do a counter
 
-        res = []
+        res = [0] * len(nums) # this did not speed it up
 
         for i in range(len(nums)):
             count = 0
             for j in range(len(nums)):
                 if i != j and nums[j] < nums[i]: 
                     count += 1
-            res.append(count)
+            res[i] = count
         return res
+
+        # to make it faster we could sort it
+        
+        
+
 
 
         
